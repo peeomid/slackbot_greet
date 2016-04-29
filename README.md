f2763240c26f120f09d140b846c95f4e84eb56e1
Macbook Pro <macbookpro@Macbooks-iMac.local>
2016-04-30 00:51:31 +0700

    send direct message instead of to a channel

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# slackbot_greet
+To deploy this Slack bot, you will need to set up a python environment.
+
+With the assumption that you already have a python environment, you'll need to:
+
+- Install required packages:
+`pip install -r requirements.txt'
+- Change the configuration in `bot_config.ini` file. More specifically, you'll need to get API key from slack team and put it in there. The current key is for the test team `ebayL3Collaboration`.
+- Run following command to connect the bot to slack, and leave it there. It would be great idea to run it as daemon.
+`python slackbot_greet.py`
+
+And now you can test out by inviting a new user, and check direct message of that user.
--- slackbot_greet.py
+++ slackbot_greet.py
@@ -9,24 +9,47 @@ except ImportError:
 # Constants
 CONFIG_FILE = 'bot_config.ini'
 
+def get_direct_channel(sc, user_id):
+    '''
+    Get direct channel id from username
+    '''
+    direct_message = sc.api_call("im.open", user=user_id)
+    channel = direct_message.get("channel")
+    if channel:        
+        channel_id = channel.get("id")
+        if channel_id:
+            return channel_id
+
+    return None
+
 def greet_new_user(sc, message):
-	if sc.rtm_connect():
-	    while True:
-			new_events = sc.rtm_read()
-			for event in new_evts:				
-		        event_type = event.get("type")
-		        event_user = event.get("user")
-		        if not event_type or not event_user:
-		            continue
-		        sc.rtm_send_message("general", message.format(user))
-	        # Sleep for half a second
+    if sc.rtm_connect():
+        print "successfully connect to the team"
+        while True:            
+            new_events = sc.rtm_read()
+            # print new_events
+            for event in new_events:                              
+                event_type = event.get("type")
+                event_user = event.get("user")                
+                if not event_type or not event_user:
+                    continue
+                if event_type == "team_join":
+                    # print event
+                    user_id = event_user.get("id")
+                    if not user_id:
+                        continue
+                    channel_id = get_direct_channel(sc, user_id)
+                    if channel_id:                    
+                        to_send = message.format(user_id)                        
+                        sc.rtm_send_message(channel_id, to_send)
+            # Sleep for a second
             time.sleep(1)
-	else:
-	    print "There's error connecting to slack, please check configuration or contact support engineer"
+    else:
+        print "There's error connecting to slack, please check configuration or contact support engineer"
 
 
 if __name__ == "__main__":
-	# Gather configuration
+    # Gather configuration
     bot_config = configparser.ConfigParser()
     bot_config.read(CONFIG_FILE)
     api_key = bot_config.get("bot", "apikey")
